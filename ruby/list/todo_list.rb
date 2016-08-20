68a60257dd453647fd192b2a414b1d2f42d000b2
Natalie Liles <natalieliles@Natalies-MacBook-Air.local>
2016-08-20 13:10:15 -0400

    Create todo list file to add todo list class and add methods.

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,21 @@
+class Todolist
+	def initialize(todolist)
+		@todolist = todolist
+	end 
+#	def add_item(item)
+#		list.push(item)
+#	end
+#	def delete_item(item)
+#		list.delete(item)
+#	end 
+#	def retrieve_item(item)
+#		list[index]
+#	end
+	def get_items
+	    todolist
+	end
+end
+
+
+
+
