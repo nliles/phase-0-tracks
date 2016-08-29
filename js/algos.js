beb096bdf8fecde054b99aedf26eb0cbf123d869
Natalie_Liles <Natalieliles@gmail.com>
2016-08-29 12:07:27 -0400

    Add function to compare key-value pairs of two hashes.

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,6 @@
 
+// 
+
 function Longest_string(array) {
     var string = 0;
     for (var i = 0; i < array.length; i++) {
@@ -7,9 +9,20 @@ function Longest_string(array) {
         longestString = array[i];
         }
     }
-    return longestString;
+    console.log (longestString);
 }
 
 Longest_string(["long phrase", "longest phrase", "longer phrase"])
+
+
+function compare_values(hash_1, hash_2) {
+  return Object.keys(hash_1).reduce(function(same_value, key) {
+    if (hash_1[key] == hash_2[key]) same_value[key] = hash_2[key];
+    return same_value;
+  }, {});
+}
+
+compare_values({'name':'Steven', 'age':'54'},{'name':'Tamir', 'age':'54'});
+
 
 
