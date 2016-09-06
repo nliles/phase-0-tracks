881d796e99fba10457c294888acc626f78c8ae52
Natalie_Liles <Natalieliles@gmail.com>
2016-09-06 10:21:39 -0400

    Create design hash based on user input.

--- ruby/hashes.rb
+++ ruby/hashes.rb
@@ -1,34 +1,32 @@
-results = ""
-print "First name:"
-first = gets.chomp
-print "Last name:"
-last = gets.chomp
-print "Enter age:"
-age = gets.to_i
-print "Number of children:"
-children = gets.to_i
-print "Decor theme:"
-decor_theme = gets.chomp
+Interior_design = {}
 
-client_data = { first: first, last: last, age: age, children: children, decor_theme: decor_theme}
-puts "#{client_data}"
+puts "What's your name?"
+Interior_design[:name] = gets.chomp 
+puts "How old are you?"
+Interior_design[:age] = gets.to_i 
+puts "How many children do you have?"
+Interior_design[:children] = gets.to_i 
+puts "Apartment or house?"
+Interior_design[:home] = gets.chomp
+puts "How many bedrooms are in your home?"
+Interior_design[:bedrooms] = gets.chomp
+puts "What is your decor theme?"
+Interior_design[:decor_theme] = gets.chomp 
 
-puts "Do you need to update your decor theme?"
-update = gets.chomp 
-if update == "yes"
-	update = true 
-elsif update == "no"
-    update = false 
-else 
-	puts "please repeat"
+
+puts "Would you like to update any information?"
+update = gets.chomp
+
+	if update == "yes"
+	 puts "What would you like to update (name, age, children, home, bedrooms,decor_theme)?"
+     new_key = gets.chomp.to_sym
+
+	 puts "Please enter your update:"
+     new_value = gets.chomp     
+     Interior_design[new_key] = new_value
+     puts Interior_design
+	
+	else 
+	puts Interior_design
+
 end 
-until update == false
-print "Decor_theme:"
-decor_theme = gets.chomp
-if decor_theme == "done"
-  break
-end
-
-client_data = { first: first, last: last, age: age, children: children, decor_theme: decor_theme}
-puts "#{client_data}"
-end
