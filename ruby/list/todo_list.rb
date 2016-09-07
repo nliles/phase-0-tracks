304eb4cb7882506acc03897e6d2718a1dd399854
Natalie_Liles <Natalieliles@gmail.com>
2016-09-07 18:34:22 -0400

    Create Todolist class with instance methods.

---
+++ ruby/list/todo_list.rb
@@ -0,0 +1,25 @@
+
+
+class Todolist
+
+def initialize(list)
+    @list = list
+end 
+
+def get_items
+	@list
+end 
+
+def add_item(item)
+    @list << item
+end 
+
+def delete_item(item)
+    @list.delete(item)
+end
+
+def get_item(index)
+    @list.at(index)
+end
+
+end
