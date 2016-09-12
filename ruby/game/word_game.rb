2e8dd014e110101d724e560d9182cdc5055d29f0
Natalie_Liles <Natalieliles@gmail.com>
2016-09-12 13:05:28 -0400

    Add pseudocode to word_game.

---
+++ ruby/game/word_game.rb
@@ -0,0 +1,10 @@
+# Design a word guessing game that takes input from two users
+# User1 enters word to be guessed.
+# User2 tries to guess that word.
+# All matching letters from the guessed word will be displayed.
+# Chances are equal to the length of the word to be guessed.
+# Game ends when chances are equal to zero.
+# The word to be guessed is compared to the guessed word to compare.
+# Matching letters are displayed.
+# If word is not guessed in the number of allowed tries, user2 loses.
+# If word is guessed, user1 wins and word is displayed.
