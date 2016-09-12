f29aa908306476ec05b4b8d3bfef10ce4c62d55f
Natalie_Liles <Natalieliles@gmail.com>
2016-09-12 13:12:31 -0400

    Add word_game class.

--- ruby/game/word_game.rb
+++ ruby/game/word_game.rb
@@ -8,3 +8,29 @@
 # Matching letters are displayed.
 # If word is not guessed in the number of allowed tries, user2 loses.
 # If word is guessed, user1 wins and word is displayed.
+
+
+class Word_game
+	attr_reader :guess_word, :word, :guess_count, :chances
+	attr_accessor :guess
+	
+	def initialize(word)
+		@word = word.downcase 
+		@guess_word = " _" * word.length 
+		@chances = word.length
+		@guess_count = 0
+		@guess = guess
+
+   end 
+
+   def user_guess(guess)
+   	   @guess = guess.split('')
+   	   user_guess.each do |x|
+   	   if @word.include?(x)
+   	   	print x 
+   	   else 
+   	   	print " _"
+      end 
+end 
+end 
+end
