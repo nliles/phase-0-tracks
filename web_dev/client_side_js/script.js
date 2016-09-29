cba54607c5bc9cedee01c7be7f654fde66055354
Natalie_Liles <Natalieliles@gmail.com>
2016-09-29 17:41:41 -0400

    update image on index.html site and add styling.

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -2,4 +2,21 @@
 console.log("running script");
 
 
+var photo = document.getElementsByTagName("img")[0];
+
+photo.style.border = "3px solid black";
+
+
+
 document.body.style.backgroundColor = "#ffb6c1";
+
+
+var body = document.getElementsByTagName("body")[0];
+
+body.style.fontFamily = "Helvetica";
+
+body.style.textAlign = "center";
+
+var head = document.getElementsByTagName("head")[0];
+
+head.style.fontFamily = "Helvetica";
